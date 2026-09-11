7ae01ef9972bd6fabe249cc6372c5d8d7cb0a455
Mali-r <ichmali.24@gmail.com>
2026-09-11 20:09:26 +0700

    Frontend: Set vite, tailwind and .env

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main className='flex flex-col items-center justify-center p-12 gap-6'>
+      <h1 className="">Hi,Mali</h1>
+      <div className="input">
+        <input type="text" name="name" placeholder="product name" />
+      </div>
+    </main>
+    </>
+  )
+}
+
+export default App
